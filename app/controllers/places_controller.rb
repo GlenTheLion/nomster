35dfe33375bc02764c15d6418b2765dd845c9f64
Glen Datugan <Glen.datugan@gmail.com>
2019-07-29 01:08:24 +0000

    lesson 24.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,12 +21,19 @@ end
   def edit
     @place = Place.find(params[:id])
   end
-  
+
 def update
   @place = Place.find(params[:id])
   @place.update_attributes(place_params)
   redirect_to root_path
 end
+
+def destroy
+  @place = Place.find(params[:id])
+  @place.destroy
+  redirect_to root_path
+end
+
 
   private
 
